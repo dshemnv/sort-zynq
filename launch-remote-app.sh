a124aed5e8c5c2a773b13b39151728b13a1dd2d7
Denis Shemonaev <denis.shemonaev@ims-bordeaux.fr>
2024-04-15 14:10:46 +0200

    Removed unnecessay files

--- launch-remote-app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BIN="$1"
-
-echo "Transfer binary to target"
-scp ${PROGRAM_PATH} root@${TARGET_IP}:/mnt/sd-mmcblk0p1
-
-echo "Launching target"
-ssh -t root@${TARGET_IP} "sh -c '.${BIN} /home/root/MOT15/test/Venice-1'"
