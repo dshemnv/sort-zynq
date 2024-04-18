bb5762881d739b672d1965bc4884ee37f35519ec
Denis Shemonaev <denis.shemonaev@ims-bordeaux.fr>
2024-04-18 15:18:11 +0200

    Fixed hardcoded device id for webcam

--- src/acqsys.cpp
+++ src/acqsys.cpp
@@ -34,7 +34,8 @@ int AqSysFiles::size() { return imgPaths.size(); }
 
 AqSysCam::AqSysCam(int devId) {
     std::string gstreamerPipeline =
-        "v4l2src device=/dev/video0 ! image/jpeg, width=1920, height=1080, "
+        "v4l2src device=/dev/video" + std::to_string(devId) +
+        " ! image/jpeg, width=1920, height=1080, "
         "framerate=30/1 ! jpegdec ! videoconvert ! appsink";
     stream = cv::VideoCapture(gstreamerPipeline, cv::CAP_GSTREAMER);
     if (!stream.isOpened()) {
